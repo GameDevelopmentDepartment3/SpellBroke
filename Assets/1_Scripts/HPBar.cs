3fb6330dc713d814f649f5a78d10e6328d3b4eb6
tlqks <jihwan5580@gmail.com>
2026-01-06 14:56:17 +0900

    UI

---
+++ Assets/1_Scripts/HPBar.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class HPBar : MonoBehaviour
+{
+    [SerializeField] private Image fillImage;
+    [SerializeField] private float maxHP = 100f;
+
+    private float currentHP;
+
+    void Start()
+    {
+        currentHP = maxHP;
+        UpdateHP();
+    }
+
+    void Update()
+    {
+        // q & e 체력 테스트
+        if (Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            TakeDamage(10f);
+        }
+
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            TakeDamage(-10f);
+        }
+        {
+            
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHP = Mathf.Clamp(currentHP - damage, 0f, maxHP);
+        UpdateHP();
+    }
+
+    private void UpdateHP()
+    {
+        fillImage.fillAmount = currentHP / maxHP;
+    }
+}
