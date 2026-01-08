506db3362227927ee2a5e97e25de483432912bfd
tlqks <jihwan5580@gmail.com>
2026-01-08 15:30:16 +0900

    증강

--- Assets/1_Scripts/Augment/AugmentButton.cs
+++ Assets/1_Scripts/Augment/AugmentButton.cs
@@ -1,36 +1,33 @@
 using UnityEngine;
 
-public enum AugmentType
-{
-    HP,
-    Attack,
-    Speed
-}
-
 public class AugmentButton : MonoBehaviour
 {
+    public enum AugmentType
+    {
+        HP,
+        Attack,
+        Speed
+    }
     public AugmentType type;
 
     [SerializeField] private PlayerStatsManager player;
     [SerializeField] private AugmentUI augmentUI;
-
     public void SelectAugment()
     {
         switch (type)
         {
             case AugmentType.HP:
-                player.AddHP(20f);
+                player.AddHP(110000f);
                 break;
-
+            
             case AugmentType.Attack:
                 player.AddAttack(5f);
                 break;
 
             case AugmentType.Speed:
-                player.AddSpeed(1f);
-                break;
+                player.AddSpeed(11000f);
+                break; 
         }
-
         augmentUI.Close(); // 선택 후 닫기
     }
 }
