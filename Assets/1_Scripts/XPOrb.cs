90f432c7e20bbcf6954f4d3c3987cc6a84705931
tlqks <jihwan5580@gmail.com>
2026-01-13 17:08:08 +0900

    speedup

--- Assets/1_Scripts/XPOrb.cs
+++ Assets/1_Scripts/XPOrb.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class XPOrb : MonoBehaviour
 {
     public int xpAmount = 10;
-    public float moveSpeed = 10f;
+    public float moveSpeed = 20f;
 
     private Transform player;
 
